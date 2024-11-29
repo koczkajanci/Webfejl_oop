1587ed9ab8e999fc0e40d372a00932438a6cdca5
Buttyan Peter Samuel <buttyan.peter@BOLYAI.LOCAL>
2024-11-29 12:54:29 +0100

    ugyanaz csak classal

--- basics/scripts.js
+++ basics/scripts.js
@@ -1,24 +1,47 @@
-function Player(nickname){
-    this.nickname = nickname
-    this.playedMatch = 0
-}
-Player.prototype.play = function(){
-    this.playedMatch++;
-    console.log(this.nickname, this.playedMatch)
-}
+// function Player(nickname){
+//     this.nickname = nickname
+//     this.playedMatch = 0
+// }
+// Player.prototype.play = function(){
+//     this.playedMatch++;
+//     console.log(this.nickname, this.playedMatch)
+// }
 
-Player.prototype.getTierLevel = function(){
-    if(this.playedMatch <= 3){
-        return 'a'
+// Player.prototype.getTierLevel = function(){
+//     if(this.playedMatch <= 3){
+//         return 'a'
+//     }
+//     else if(this.playedMatch <3 && this.playedMatch <= 5){
+//         return 'b'
+//     }
+//     else{
+//         return 'c'
+//     }
+// }
+
+
+class Player {
+    constructor(nickname) {
+        this.nickname = nickname
+        this.playedMatch = 0
     }
-    else if(this.playedMatch <3 && this.playedMatch <= 5){
-        return 'b'
+
+    play() {
+        this.playedMatch++;
+        console.log(this.nickname, this.playedMatch)
     }
-    else{
-        return 'c'
+    getTierLevel() {
+        if (this.playedMatch <= 3) {
+            return 'a'
+        }
+        else if (this.playedMatch < 3 && this.playedMatch <= 5) {
+            return 'b'
+        }
+        else {
+            return 'c'
+        }
     }
 }
-
 
 const gomszab = new Player("gomszab")
 
