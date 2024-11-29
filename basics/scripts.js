1d493bbf6a65fd9387ed632b07d913c2a77125eb
Buttyan Peter Samuel <buttyan.peter@BOLYAI.LOCAL>
2024-11-29 12:46:33 +0100

    legjobb ora! *boldogsag = Math.Max

---
+++ basics/scripts.js
@@ -0,0 +1,29 @@
+function Player(nickname){
+    this.nickname = nickname
+    this.playedMatch = 0
+}
+Player.prototype.play = function(){
+    this.playedMatch++;
+    console.log(this.nickname, this.playedMatch)
+}
+
+Player.prototype.getTierLevel = function(){
+    if(this.playedMatch <= 3){
+        return 'a'
+    }
+    else if(this.playedMatch <3 && this.playedMatch <= 5){
+        return 'b'
+    }
+    else{
+        return 'c'
+    }
+}
+
+
+const gomszab = new Player("gomszab")
+
+console.log(gomszab)
+gomszab.play()
+console.log(gomszab.getTierLevel())
+
+
