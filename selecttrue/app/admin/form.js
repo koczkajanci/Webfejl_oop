133dfac2e4bb7f38699fe3519e68b8aa2be51d72
Buttyan Peter Samuel <buttyan.peter@bolyai.local>
2025-03-07 12:26:51 +0100

    orai elso

---
+++ selecttrue/app/admin/form.js
@@ -0,0 +1,170 @@
+/**
+ * Manageli es rendereli a formunkat a field segitsegevelS
+ * 
+ */
+
+class FormController{
+    /**
+     * @type {Manager} 
+     */
+    #manager;
+    /**
+     * @type {FormField[]}
+     * 
+     */
+    #formFieldArray
+    /**
+     * 
+     * @param {Manager} manager 
+     * @param {{label: string, type: string, id: string, id:string, optional?:boolean}[]} fieldConfiguration 
+     */
+    constructor(manager, fieldConfiguration){
+        this.#formFieldArray = []
+        this.#manager
+        const form = document.createElement('form')
+        document.body.appendChild(form)
+        for(const field of fieldConfiguration){
+            const formField = new FormField(field.id, field.label, field.type, field.optional)
+            this.#formFieldArray.push(formField)
+            form.appendChild(formField.getDivElement())
+        }
+        const sendButton = document.createElement('button');
+        sendButton.textContent = 'Elküld';
+        form.appendChild(sendButton)
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            if(this.#validateFields()){
+                const value = this.#getValueObject();
+            }
+            //validaljuk a fieldeket, megjeenitjuk errorokat
+            //elkerjuk a fieldek ertekeit    
+        })        
+
+    }
+
+    /** 
+     * @returns {boolean} igazzal ter vissza ha minden field helyesen van kitoltve
+     * 
+     */
+    #validateFields(){
+        let valid = true;
+        for(const formField of this.#formFieldArray){
+            formField.error = '';
+            if(!formField.optional){
+                if(formField.value == ''){
+                    formField.error = 'A mezo kitoltese kotelezo'
+                    valid = false
+                }
+            }
+        }
+        return valid;
+    }
+    /**
+     * Vegigmegy a formfieldeken es id-hoz
+     * rendeli az ertekeket
+     * @returns {}
+     */
+    #getValueObject(){
+        const result = {};
+        for(const formField of this.#formFieldArray){
+            result[formField.id] = formField.value                                                                                                                                                                                                                                                                                                                                                                                                                             
+        }
+        console.log(result)
+        return result;
+        
+    }
+}
+
+/**
+ * Ez fogja tartalmazni a labeleket, inputokat errorspanokat
+ * pl.: text input alatta errorspan
+ */
+class FormField{
+    /**
+     * @type {string}
+     */
+    #id
+
+    /** 
+     * @type {string}
+     */
+    #type
+
+    /**
+     * @type {boolean}
+     */
+    #optional
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    #inputField
+
+    /**
+     * @type {HTMLLabelElement}
+     */
+    #labelElement
+
+
+    /**
+     * @type {HTMLSpanElement}
+     */
+    #errorField
+
+
+    get id(){
+        return this.#id;
+    }
+    /**
+     * Ha az input tipusa checkbox, 
+     * akkor visszater azzal, hogy be van pipalva,
+     * vagy sem , ha pedig text, akkor visszater a szoveggel
+     * 
+     * 
+     * @returns {boolean||string}
+     */
+    get value(){
+        if(this.#type === 'checkbox'){
+            return this.#inputField.checked
+        }
+        return this.#inputField.value
+    }   
+
+    get optional(){
+        return this.#optional;
+    }
+
+    set error(value){
+        this.#errorField.textContent = value
+    }
+    
+
+    /**
+     * 
+     * @param {string} id 
+     * @param {string} labelContent 
+     * @param {string} type 
+     * @param {boolean?} optional 
+     */
+    constructor(id, labelContent, type, optional = false){
+        this.#id = id;
+        this.#type = type;
+        this.#optional = optional;
+        this.#labelElement = Gomszab.makeLabel(id, labelContent);
+        this.#inputField = Gomszab.makeInput(id,type);
+        this.#errorField = Gomszab.makeErrorField();
+    }
+
+    /**
+     * 
+     * letrehoz egy div elementet, amiben benne van az input,
+     *  a label, az errorspan
+     * 
+     * @returns {HTMLDivElement}
+     */
+    getDivElement(){
+        const fields = [this.#labelElement, this.#inputField, this.#errorField]
+        const div =  Gomszab.makeDiv(fields)
+        return div;
+    }
+}
--- selecttrue/app/admin/main.js
+++ selecttrue/app/admin/main.js
@@ -2,3 +2,6 @@ const formFieldConfiguration = [
     {id: 'cardtext', label: 'allitas', type: 'text'},
     {id: 'right', label: 'igaz?', type: 'checkbox', optional: true},
 ]
+
+const manager = new Manager();
+const formController = new FormController(manager,formFieldConfiguration);
